dafa578949cabbec45f20071583bec323b11206c
Bhashkarjya Nath <bhashkarjyanath1999@gmail.com>
2022-04-14 00:42:16 +0530

    backend completed

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const Blockchain = require('./blockchain/blockchain');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const PubSub = require('./app/pubsub');
+const TransactionMiner = require('./app/transaction-miner'); 
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({blockchain, transactionPool, wallet});
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 3000;
 const ROOT_ADDRESS_NODE =  `http://localhost:${DEFAULT_PORT}`;
@@ -37,7 +39,11 @@ app.post('/api/transact', (req,res) => {
             transaction.update({senderWallet: wallet, recipient, amount});
         }
         else{
-            transaction = wallet.createTransaction({recipient, amount});
+            transaction = wallet.createTransaction({
+                recipient,
+                amount,
+                chain: blockchain.chain
+            });
         }
     } catch(error){
         return res.status(400).json({type: 'error', message: error.message})
@@ -53,6 +59,20 @@ app.post('/api/transact', (req,res) => {
 app.get('/api/transaction-pool-map', (req,res) => {
     res.json(transactionPool.transactionMap);
 });
+
+app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
+    
+    res.redirect('/api/blocks');
+});
+
+app.get('/api/wallet-info', (req, res) => {
+    const address = wallet.publicKey;
+    res.json({ 
+        address,
+        balance: Wallet.calculateBalance({ chain: blockchain.chain, address})
+    });
+})
 
 var PEER_PORT;
 
